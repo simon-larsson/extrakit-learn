2735f51c11a56badfb08756344ce7ca86e8a1f22
Simon Larsson <simon.larsson@seavus.com>
2019-05-12 18:06:51 +0200

    Fixed count encoder

--- xklearn/preprocessing/count_encoder.py
+++ xklearn/preprocessing/count_encoder.py
@@ -9,7 +9,7 @@
 '''
 
 from warnings import warn
-from ..preprocessing.util import check_error_strat
+from ..preprocessing.util import check_error_strat, is_float_array
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import column_or_1d, check_is_fitted
 import numpy as np
@@ -39,17 +39,8 @@ class CountEncoder(BaseEstimator, TransformerMixin):
             raise ValueError('Value of `missing` {} is not a valid replacement '
                              'strategy, {}'.format(missing, replace_strats))
 
-        if unseen == 'one':
-            self.default_unseen_ = 1
-        elif unseen == 'nan':
-            self.default_unseen_ = np.nan
-
-        if missing == 'one':
-            self.default_missing_ = 1
-        elif missing == 'nan':
-            self.default_missing_ = np.nan
-
-        self.requires_float_ = missing == 'nan' or unseen == 'nan'
+        self.default_unseen_ = strat_to_default(unseen)
+        self.default_missing_ = strat_to_default(missing)
 
         self.unseen = unseen
         self.missing = missing
@@ -74,12 +65,10 @@ class CountEncoder(BaseEstimator, TransformerMixin):
 
         X = column_or_1d(X, warn=True)
 
-        if self.requires_float_:
-            X = X.astype('float64')
-
         check_error_strat(np.isnan(X), self.missing, 'missing')
 
-        self.classes_, self.counts_ = np.unique(X, return_counts=True)
+        self.classes_, self.counts_ = np.unique(X[np.isfinite(X)], 
+                                                return_counts=True)
 
         if self.classes_.shape[0] != np.unique(self.counts_).shape[0]:
             warn('Duplicate count encoding for muliple classes. This will '
@@ -110,11 +99,10 @@ class CountEncoder(BaseEstimator, TransformerMixin):
         check_is_fitted(self, 'classes_')
         X = column_or_1d(X, warn=True)
 
-        if self.requires_float_:
-            X = X.astype('float64')
-
         missing_mask = np.isnan(X)
-        unseen_mask = np.isin(X, self.classes_, invert=True)
+        encode_mask = np.invert(missing_mask)
+        unseen_mask = np.bitwise_xor(np.isin(X, self.classes_, invert=True), 
+                                     missing_mask)
 
         check_error_strat(missing_mask, self.missing, 'missing')
         check_error_strat(unseen_mask, self.unseen, 'unseen')
@@ -122,17 +110,28 @@ class CountEncoder(BaseEstimator, TransformerMixin):
         # Make all unseen to the same class outside of classes
         X[unseen_mask] = np.max(self.classes_)
 
-        _, indices = np.unique(X, return_inverse=True)
+        _, indices = np.unique(X[encode_mask], return_inverse=True)
 
         # Perform replacement with lookup
-        X = np.take(self.lut_[:, 1], \
-                    np.take(np.searchsorted(self.lut_[:, 0], self.classes_),
-                            indices))
+        X[encode_mask] = np.take(self.lut_[:, 1], \
+                                 np.take(np.searchsorted(self.lut_[:, 0], 
+                                                         self.classes_),
+                                         indices))
 
         if np.any(missing_mask):
             X[missing_mask] = self.default_missing_
 
-        if self.requires_float_ and np.all(np.isfinite(X)):
+        # Cast as int if possible
+        if is_float_array(X) and np.all(np.isfinite(X)):
             X = X.astype('int64')
 
         return X
+
+def strat_to_default(strat):
+    
+    if strat == 'one':
+        return 1
+    elif strat == 'nan':
+        return np.nan
+    else:
+        return None
